a854ca2e575cacc2d91466dc4f1f761c72700bb1
Maryna Artiushchenko <m.artiushchenko@gmail.com>
2024-03-28 14:58:43 -0400

    update

--- src/components/MovieListContent/MovieListContent.jsx
+++ src/components/MovieListContent/MovieListContent.jsx
@@ -1,15 +1,14 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 const MovieListContent = ({ movie }) => {
+  const location = useLocation();
   if (!movie.poster_path) {
     return null;
   }
 
-console.log(movie.id); 
-
   return (
     <li>
-      <Link to={`/movies/${movie.id}`}>
+      <Link to={`/movies/${movie.id}`} state={location}>
         <p>{movie.title}</p>
       </Link>
     </li>
